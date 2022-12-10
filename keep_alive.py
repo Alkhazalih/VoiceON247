a4abe97fa5b6e863de5e30b73097965b3919f67e
Sanad Khazalih <99549578+MrTwix450@users.noreply.github.com>
2022-12-10 16:39:29 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return '<meta http-equiv="refresh" content="0; URL=https://store.mrtwix.repl.co"/>'
+    return '<meta http-equiv="refresh" content="0; URL=https://www.jotform.com/app/223421904300441/page/0"/>'
 
 def run():
     app.run(host="0.0.0.0", port=8080)
